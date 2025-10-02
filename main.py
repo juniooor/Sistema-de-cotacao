2f288a439f8f32296bfff2189491ec85cf82e7c0
juniooor <juniormodern@hotmail.com>
2025-10-02 20:56:49 -0300

    estruturando

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 print("hello World")
 
 
-nome_empresa = input('digite nome da empresa: ')
-nome_produto = input('digite nome do produto: ')
-nome_valor = input('digite nome produto: ')
-frete = input('digite valor do frete: ')
+nomeEmpresa = input('digite nome da empresa: ')  # nome do fornecedor do item
+# nome do item depois pretendo criar categoria como manutenção ou desgaste
+nomeProduto = input('digite nome do produto: ')
+# valor do produto livre de impostos
+valorProduto = int(input('digite valor do produto: '))
+# valor já calculado pela transportadora dps faço uma base de calculo baseado no peso x valor NF
+frete = int(input('digite valor do frete: '))
+# tenho que fazer o calculo de de ipi e icms e aliquota , mas por hora vai ser um valor já definido pelo user
+imposto = int(input("imposto:"))
+prazo = int(input("prazo:"))  # prazo para chegar a peça ao cliente
+
+cet = sum([valorProduto, frete, imposto])
+
+print(cet)
