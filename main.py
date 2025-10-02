84b57b53d22dcdd412f88446c55924c786aafc5a
juniooor <juniormodern@hotmail.com>
2025-10-02 20:27:30 -0300

    testes

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("hello World")
+
+
+nome_empresa = input('digite nome da empresa: ')
+nome_produto = input('digite nome do produto: ')
+nome_valor = input('digite nome produto: ')
+frete = input('digite valor do frete: ')
